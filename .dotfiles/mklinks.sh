b73798cd94825a104a98e5a256dd5d3ce5e7c195
Deyn0524 <64955397+Deyn05241@users.noreply.github.com>
2025-06-29 13:53:08 +0400

    - Modified .config/niri/config.kdl: Moved custom keybinds to end of file; Added keybind to open a file explorer. - Modified setup.sh: Fixed formatting of header block; Fixed a bug where cache foldere was not created if yay was detected on the system; Removed unnecessary cd and mkdir commands; Fixed formatting of several titles; Fixed formatting of several if statements; Script now automatically themes GTK, Icons and Cursors; Fixed formatting for symlink creation part; Added reboot reminder at script completion.

--- .dotfiles/mklinks.sh
+++ .dotfiles/mklinks.sh
@@ -19,8 +19,20 @@ fi
 ln -s $HOME/.dotfiles/dot-zsh-theme/.p10k.zsh $HOME/.p10k.zsh
 
 # Symlink for configs
-rm -f $HOME/.config/niri/config.kdl
-rm -f $HOME/.config/foot/foot.ini
-rm -f $HOME/.config/waybar/config.jsonc
-rm -f $HOME/.config/waybar/.style.css
+if [ -f "$HOME/.config/niri/config.kdl" ]
+then
+    mv $HOME/.config/niri/config.kdl $HOME/.config/niri/config.kdl.bak
+fi
+if [ -f "$HOME/.config/foot/foot.ini" ]
+then
+    mv $HOME/.config/foot/foot.ini $HOME/.config/foot/foot.ini.bak
+fi
+if [ -f "$HOME/.config/waybar/config.jsonc" ]
+then
+    mv $HOME/.config/waybar/config.jsonc $HOME/.config/waybar/config.jsonc.bak
+fi
+if [ -f "$HOME/.config/waybar/style.css" ]
+then
+    mv $HOME/.config/waybar/style.css $HOME/.config/waybar/style.css.bak
+fi
 cp -rs $HOME/.dotfiles/.config/* $HOME/.config/
--- .dotfiles/setup.sh
+++ .dotfiles/setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
-###############################################################################################
-# Script to setup Deyn0524's dotfiles for Niri, a scrollable tiling window manager for Wayland#
-###############################################################################################
+################################################################################################
+# Script to setup Deyn0524's dotfiles for Niri, a scrollable tiling window manager for Wayland #
+################################################################################################
+
+# Create cache folder to store temporary files
+echo "-----Creating cache folder for temporary files-----"
+echo ""
+mkdir -p $HOME/.cache/deyn0524-niri-dots/
+echo "---Cache folder created!---"
+echo ""
 
 # Check if needed programs are already installed, if not, installs it
 echo "-----Installing necessary programs-----"
@@ -25,14 +32,10 @@ echo ""
 if ! command -v yay > /dev/null 2>&1
 then
     echo "---yay could not be found, installing...---"
-    cd $HOME/.cache
-    mkdir deyn0524-niri-dots
-    cd deyn0524-niri-dots
+    cd $HOME/.cache/deyn0524-niri-dots
     git clone https://aur.archlinux.org/yay.git
     cd yay
     makepkg -si
-    cd $HOME/.cache
-    rm -rf deyn0524-niri-dots
 else
     echo "---yay already installed!---"
 fi
@@ -69,10 +72,10 @@ echo ""
 echo "-----Theming zsh-----"
 echo ""
 
-echo "---Installing zinit"
+echo "---Installing zinit---"
 echo ""
 
-if ! pacman -Qs zinit > /dev/null 2&>1
+if ! pacman -Qs zinit > /dev/null 2>&1
 then
     yay -S zinit --answerclean none --answerdiff none --noconfirm
 else
@@ -85,11 +88,75 @@ wget -O everforest-dark.zsh https://raw.githubusercontent.com/sainnhe/dotfiles/r
 echo "-----Done downloading!-----"
 echo ""
 
+# Theming GTK, Icons and Cursors
+echo "-----Theming GTK, Icons and Cursors-----"
+echo ""
+
+echo "---Theming GTK---"
+echo ""
+echo "---Cloning repository---"
+echo ""
+cd $HOME/.cache/deyn0524-niri-dots
+git clone https://github.com/Fausto-Korpsvart/Everforest-GTK-Theme
+echo "---Installing prerequisites---"
+echo ""
+if ! pacman -Qs gtk-engine-murrine > /dev/null 2>&1
+then
+    sudo pacman -S gtk-engine-murrine --noconfirm
+else
+    echo "---Prerequisites already installed!---"
+fi
+echo ""
+echo "---Installing GTK theme---"
+echo ""
+$HOME/.cache/deyn0524-niri-dots/Everforest-GTK-Theme/themes/install.sh -l 
+gsettings set org.gnome.desktop.interface gtk-theme 'Everforest-Dark'
+echo "---GTK theme installed!---"
+echo ""
+echo "---Fixing Flatpak theming---"
+echo ""
+echo "---Installing prerequisites---"
+echo ""
+if ! pacman -Qe flatpak > /dev/null 2>&1
+then
+    sudo pacman -S flatpak ostree appstream-glib --noconfirm
+else
+    echo "---Prerequisites already installed!---"
+fi
+echo ""
+echo "---Cloning repository---"
+echo ""
+git clone https://github.com/refi64/stylepak
+cd stylepak
+./stylepak install-user Everforest-Dark
+echo "---Flatpak theming fixed!---"
+echo ""
+
+echo "---Theming Icons---"
+echo ""
+mkdir -p $HOME/.icons/
+mv $HOME/.cache/deyn0524-niri-dots/Everforest-GTK-Theme/icons/Everforest-Dark $HOME/.icons/
+gsettings set org.gnome.desktop.interface icon-theme 'Everforest-Dark'
+echo "---Icons themed!---"
+echo ""
+
+echo "---Theming Cursors---"
+echo ""
+echo "---Downloading Cursors---"
+echo ""
+wget -cO- https://github.com/talwat/everforest-cursors/releases/latest/download/everforest-cursors-variants.tar.bz2 | tar xfj - -C ~/.icons
+gsettings set org.gnome.desktop.interface cursor-theme everforest-cursors-light
+gsettings set org.gnome.desktop.interface cursor-size 32
+echo "---Cursors themed!---"
+echo ""
+
 # Runs mklink.sh to automatically create symlinks
 cd $HOME/.dotfiles
-echo -----Creating Symlinks for configuration files-----
-echo Running mklinks.sh
+echo "-----Creating Symlinks for configuration files-----"
+echo ""
 ./mklinks.sh
-echo -----Done creating symlinks!-----
+echo "-----Done creating symlinks!-----"
 
+echo "-----SCRIPT COMPLETE!-----"
+echo "Reboot and select Niri in GDM to enjoy the dotfiles."
 
